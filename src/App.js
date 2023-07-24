10989c5f3f200146a08f76637d0e339dafd4b61c
dikikoza <zoran.filip.popov@gmail.com>
2023-07-24 14:01:26 +0200

    komitanje

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import Form from "./components/Form";
 import UserDetails from "./components/UserDetails";
+import "./App.css";
 
 const App = () => {
   const [userData, setUserData] = useState(null);
   const [userRepos, setUserRepos] = useState([]);
 
-  const handleSubmitForm = async (username) => {
-    try {
-      const userResponse = await fetch(
-        `https://api.github.com/users/${username}`
-      );
-      const user = await userResponse.json();
+  const handleSubmitForm = (username) => {
+    fetch(`https://api.github.com/users/${username}`)
+      .then((userResponse) => userResponse.json())
+      .then((user) => {
+        fetch(`https://api.github.com/users/${username}/repos`)
+          .then((reposResponse) => reposResponse.json())
+          .then((repos) => {
+            setUserData(user);
+            setUserRepos(repos);
+          })
+          .catch((error) =>
+            console.error("Error fetching user repositories:", error)
+          );
+      })
+      .catch((error) => console.error("Error fetching user data:", error));
 
-      const reposResponse = await fetch(
-        `https://api.github.com/users/${username}/repos`
-      );
-      const repos = await reposResponse.json();
-
-      setUserData(user);
-      setUserRepos(repos);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+    document.body.classList.remove("loaded"); // Remove the "loaded" class when fetching user data
   };
 
   const handleReset = () => {
     setUserData(null);
     setUserRepos([]);
+    document.body.classList.remove("loaded");
   };
 
   return (
-    <div>
+    <div className="container">
+      <div className="github-icon">
+        <FontAwesomeIcon icon={faGithub} />
+      </div>
       {!userData ? (
         <Form onSubmit={handleSubmitForm} />
       ) : (
